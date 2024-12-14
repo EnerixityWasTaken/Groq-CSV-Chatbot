6617cb2fc07fb42470a47950a4e15778a3277c2b
Moahmmed Alhabahbeh <165299141+EnerixityWasTaken@users.noreply.github.com>
2024-12-14 20:02:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 import streamlit as st
 import pandas as pd
+from dotenv import load_dotenv
 from langchain_experimental.agents import create_csv_agent
 from langchain_groq import ChatGroq
 
-def return_csv_agent(query,csv_file):
+def return_csv_agent(user_query, csv_file):
     csv_file.seek(0)
-    llm = ChatGroq(model = "llama-3.1-8b-instant",api_key = "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")
+    llm = ChatGroq(model='llama-3.1-8b-instant')
+    
     agent = create_csv_agent(llm, csv_file, verbose=True, allow_dangerous_code=True)
-    response = agent.run(query)
+    
+    response = agent.run(user_query)
     return response
-def main():
-    st.set_page_config(page_title="CSV agent",page_icon=":robot_face:")
-    st.title("CSV Agent")
 
-    csv_file = st.file_uploader("Upload CSV file",type = ['csv'])
+
+def main():
+    load_dotenv()
+    
+    st.set_page_config(page_title='CSV Agent', page_icon=':robot_face:', layout='wide')
+    st.title('CSV Agent')
+    
+    
+    csv_file = st.sidebar.file_uploader('Uploadd CSV file', type=['csv'])
     if csv_file:
+        st.sidebar.subheader('## CSV File')
         df = pd.read_csv(csv_file)
         st.sidebar.dataframe(df)
-        query = st.chat_input("Ask a question about your CSV file")
-        if query:
-            response = return_csv_agent(query,csv_file)
-
-            with st.chat_message("user"):
-                st.write(query)
-            with st.chat_message("assistant"):
+        user_query = st.chat_input('Ask a question about your CSV file')       
+        if user_query:            
+            with st.chat_message('user'):
+                st.write(user_query)
+            response = return_csv_agent(user_query=user_query, csv_file=csv_file)
+            with st.chat_message('assistant'):
                 st.write(response)
-    
-main()
 
-    
-    
+
+
+main()
